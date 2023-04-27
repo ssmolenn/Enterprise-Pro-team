ba689552cfac90ff0aa78d73375118316d6b66e5
gkapaya <124080697+gkapaya@users.noreply.github.com>
2023-04-27 05:15:44 +0100

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,35 @@
+$host = '35.189.98.101';
+$db   = 'data';
+$user = 'root';
+$pass = 'admin';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE email = ?');
+    $stmt->execute([$email]);
+    $user = $stmt->fetch();
+
+    if ($user && password_verify($password, $user['password'])) {
+        // Login successful, redirect to homepage or display success message
+        header('Location: /homepage.php');
+        exit();
+    } else {
+        // Login failed, display error message
+        echo '<div class="alert alert-danger">Incorrect email or password</div>';
+    }
+}
